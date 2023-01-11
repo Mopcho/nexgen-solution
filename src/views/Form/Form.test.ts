628ec56e81074a302244fd07f4981244060cdd33
Mopcho <valeri_petkov01@abv.bg>
2023-01-11 14:47:11 +0200

    Added more tests

--- src/views/Form/Form.test.ts
+++ src/views/Form/Form.test.ts
@@ -51,3 +51,38 @@ test('onSubmit should set form errors to false when the form data is valid', () 
   expect(model.state.formErrors.firstName).toBe(false);
   expect(model.state.formErrors.lastName).toBe(false);
 });
+
+test('onSubmit should check email correctly and sets error message for invalid email', () => {
+  const model = new FormModel();
+  const controller = new FormController({ model, pasteBinUrl: '' });
+  model.setData({
+    ...model.state.formData,
+    email: 'john',
+  });
+  controller.onSubmit({ preventDefault: jest.fn() } as unknown as React.FormEvent);
+  expect(model.state.formErrors.email).toBe('Must be a valid email');
+});
+
+test('onSubmit should check age correctly and sets error message for age under 18', () => {
+  const model = new FormModel();
+  const controller = new FormController({ model, pasteBinUrl: '' });
+  const dob = new Date().toISOString().slice(0, 10);
+  model.setData({
+    ...model.state.formData,
+    dob,
+  });
+  controller.onSubmit({ preventDefault: jest.fn() } as unknown as React.FormEvent);
+  expect(model.state.formErrors.dob).toBe('Must be at least 18 years old');
+});
+
+test('onSubmit should check for empty fields correctly and sets error messages', () => {
+  const model = new FormModel();
+  const controller = new FormController({ model, pasteBinUrl: '' });
+  controller.onSubmit({ preventDefault: jest.fn() } as unknown as React.FormEvent);
+  expect(model.state.formErrors.firstName).toBe('Must be at least 3 characters long');
+  expect(model.state.formErrors.lastName).toBe('Must be at least 3 characters long');
+  expect(model.state.formErrors.email).toBe('Must be a valid email');
+  expect(model.state.formErrors.password).toBe('Must be at least 8 characters long');
+  expect(model.state.formErrors.biography).toBe('Must be at least 20 characters long');
+  expect(model.state.formErrors.dob).toBe('Must be at least 18 years old');
+});
