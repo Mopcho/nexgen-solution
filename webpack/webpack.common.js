40a6eba37b726624ed2ab3460beb21e2e26048f3
Mopcho <valeri_petkov01@abv.bg>
2023-01-11 22:11:36 +0200

    Adding favicon

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -41,6 +41,7 @@ module.exports = {
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: path.resolve(__dirname, '..', './src/index.html'),
+			favicon: path.resolve(__dirname, '..', './assets/images/favicon.svg')
 		}),
 	],
 };
